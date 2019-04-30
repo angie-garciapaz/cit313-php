e7bb7a2760712c10f5a614fb6adb17ca2bd639a0
Angie Garcia-Paz <a_veites@Angies-MacBook-Pro.local>
2019-04-30 16:34:25 -0600

    added login

---
+++ web/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login In</title>
+    <link rel="stylesheet" href="">
+
+</head>
+
+<body>
+<h1>Login</h1>
+<a href="#" class="button">Log in as Admin</a>
+<a href="#" class="button">Log in as Tester</a>
+    <script src=""></script>
+</body>
+</html>
