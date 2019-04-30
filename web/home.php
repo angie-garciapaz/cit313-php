d2063da8c7caf8192aff62e91033dc5af6b91b71
Angie Garcia-Paz <a_veites@Angies-MacBook-Pro.local>
2019-04-30 16:32:05 -0600

    added home

---
+++ web/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+<h1>Welcome</h1>
+<p>You are not logged in.</p>
+    <script src=""></script>
+</body>
+</html>
