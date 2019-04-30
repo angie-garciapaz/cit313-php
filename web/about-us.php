adb9a2cc532d85a53366de2fddd7b36516979cd9
Angie Garcia-Paz <a_veites@Angies-MacBook-Pro.local>
2019-04-30 16:29:26 -0600

    added about us

---
+++ web/about-us.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navy's Diapers</title>
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+<h1>Navy's Diapers</h1>
+<p>We believe in sustainable products and protecting the earth. Our diapers reduce carbon foot prints and made from recycled materials.</p>
+    <script src=""></script>
+</body>
+</html>
