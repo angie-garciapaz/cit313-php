b41d6c7be6ae4e28de3ecf5f2ce57ffd98dbd215
Angie Garcia-Paz <a_veites@Angies-MacBook-Pro.local>
2019-05-08 14:49:47 -0600

    css

--- web/TA-master/submitted.php
+++ web/TA-master/submitted.php
@@ -2,54 +2,62 @@
 $nameErr = $emailErr = $majorErr = $commentsErr = "";
 $name = $email = $major = $comments = "";
 
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = $_POST["name"];
-  }
+if (empty($_POST["name"])) {
+  $nameErr = "Name is required";
+} else {
+  $name = $_POST["name"];
+}
 
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = $_POST["email"];
-  }
+if (empty($_POST["email"])) {
+  $emailErr = "Email is required";
+} else {
+  $email = $_POST["email"];
+}
 
-  if (empty($_POST["major"])) {
-    $majorErr = "Major is required";
-  } else {
-    $major = $_POST["major"];
-  }
+if (empty($_POST["major"])) {
+  $majorErr = "Major is required";
+} else {
+  $major = $_POST["major"];
+}
 
-    $continents = $_POST["continents"];
+$continents = $_POST["continents"];
 
 
-  if (empty($_POST["comments"])) {
-    $comments = "";
-  } else {
-    $comments = $_POST["comments"];
-  }
+if (empty($_POST["comments"])) {
+  $comments = "";
+} else {
+  $comments = $_POST["comments"];
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="main.css" />
-    <title>Document</title>
-</head>
-<body>
-<p>Name: <?php echo $name ?></p>
-<p>Where you've visited:
- <?php 
- foreach ($continents as $continent) {
-    echo $continent.", ";
-}
- ?>
- </p>
- <p>Major: <?php echo $major ?> </p>
-</body>
-</html>
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="main.css" />
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="jumbotron">
+    <p>Name: <?php echo $name ?></p>
+    <p>Where you've visited:
+      <?php
+      foreach ($continents as $continent) {
+        if ($continenets > 1)
+        echo $continent . ", ";
+        else
+        echo $continent;
+      }
+      ?>
+    </p>
+    <p>Major: <?php echo $major ?> </p>
+    <p>Comments: <?php echo $comments?></p>
+  </div>
+</body>
+
+</html>
