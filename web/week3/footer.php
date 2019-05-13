a57741418ce86b80328c41eaedf0c2982a4a2c3b
Angie Garcia-Paz <a_veites@Angies-MacBook-Pro.local>
2019-05-13 17:41:50 -0600

    c

---
+++ web/week3/footer.php
@@ -0,0 +1,7 @@
+<!-- Footer -->
+<footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Curry on the Corner 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
