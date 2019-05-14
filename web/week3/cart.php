031036afc69f589b2b8fa76d519b4b66906d8de7
Angie Garcia-Paz <a_veites@Angies-MacBook-Pro.local>
2019-05-13 21:45:29 -0600

    css

--- web/week3/cart.php
+++ web/week3/cart.php
@@ -2,9 +2,48 @@
 session_start();
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Curry on the Corner</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/shop-homepage.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <?php include 'nav.php'; ?>
+
+
 <h1>you've ordered</h1>
 
 <?php
 // Echo session variables that were set on previous page
 echo $_SESSION["meal1"] . ".<br>";
 ?>
+
+</body>
+
+  <!-- /.container -->
+  <?php include 'footer.php'; ?>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
