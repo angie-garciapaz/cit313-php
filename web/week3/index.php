bc9c164da7f55e8305c3745c5bdc5a0c77ea08d5
Angie Garcia-Paz <a_veites@Angies-MacBook-Pro.local>
2019-05-13 21:03:10 -0600

    css

--- web/week3/index.php
+++ web/week3/index.php
@@ -87,7 +87,7 @@ session_start()
                 </h4>
                 <h5>$14.99</h5>
                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                <a href="cart.php" class="btn btn-primary btn-lg">Add to Cart</a>
+                <a href="cart.php" class="btn btn-warning btn-lg">Add to Cart</a>
               </div>
               <div class="card-footer">
                 <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
@@ -105,6 +105,7 @@ session_start()
                 </h4>
                 <h5>$9.99</h5>
                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+                <a href="cart.php" class="btn btn-warning btn-lg">Add to Cart</a>
               </div>
               <div class="card-footer">
                 <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
@@ -121,6 +122,7 @@ session_start()
                 </h4>
                 <h5>$4.99</h5>
                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                <a href="cart.php" class="btn btn-warning btn-lg">Add to Cart</a>
               </div>
               <div class="card-footer">
                 <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
@@ -135,8 +137,9 @@ session_start()
                 <h4 class="card-title">
                   <a href="#">Chicken Tikka Masala</a>
                 </h4>
-                <h5>$24.99</h5>
+                <h5>$13.99</h5>
                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                <a href="cart.php" class="btn btn-warning btn-lg">Add to Cart</a>
               </div>
               <div class="card-footer">
                 <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
@@ -153,6 +156,7 @@ session_start()
                 </h4>
                 <h5>$3.99</h5>
                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+                <a href="cart.php" class="btn btn-warning btn-lg">Add to Cart</a>
               </div>
               <div class="card-footer">
                 <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
@@ -169,6 +173,7 @@ session_start()
                 </h4>
                 <h5>$14.99</h5>
                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                <a href="cart.php" class="btn btn-warning btn-lg">Add to Cart</a>
               </div>
               <div class="card-footer">
                 <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
